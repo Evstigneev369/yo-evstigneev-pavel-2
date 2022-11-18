800f3139ba8f0c72e58095a6a0fcc2c7fd09a10f
Павел <110538378+Evstigneev369@users.noreply.github.com>
2022-11-18 15:07:07 +0400

    ДЗ 2 модуль.
    БЭМ

--- source/js/index.js
+++ source/js/index.js
@@ -1,20 +1,20 @@
 var popup = document.querySelector('.popup')
-var popupbtn = document.querySelector('.btn-js')
+var popupbtn = document.querySelector('.button-link--js')
 var popupclose = document.querySelector('.popup__cross')
 var input = popup.querySelector('input')
 
 popupbtn.addEventListener('click', function() {
-    popup.classList.add('popup-open')
+    popup.classList.add('popup__open')
     input.focus()
 })
 
 popupclose.addEventListener('click', function() {
-    popup.classList.remove('popup-open')
+    popup.classList.remove('popup__open')
 })
 
 window.addEventListener('keydown', function(event) {
-    if (event.key === "Escape" && popup.classList.contains("popup-open")) {
-        popup.classList.remove('popup-open');
+    if (event.key === "Escape" && popup.classList.contains("popup__open")) {
+        popup.classList.remove('popup__open');
         console.logo("Закрытие формы")
     }
 })
